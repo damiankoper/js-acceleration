e684b6a5cdc4cda65f48ca983d261560c247833d
damiankoper <kopernickk@gmail.com>
2021-12-07 00:25:07 +0100

    fix: performance timer fix

--- packages/benchmark/src/timers/PerformanceTimer.ts
+++ packages/benchmark/src/timers/PerformanceTimer.ts
@@ -1,18 +1,35 @@
 import { ITimer } from "../contracts/ITimer";
 
+function getPerformance() {
+  if (typeof window !== "undefined") return window.performance;
+  else if (typeof global !== "undefined") {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    return require("perf_hooks").performance;
+  }
+}
+
 export class PerformanceTimer implements ITimer {
   public static readonly resolution = 1e6;
+  public performance?: Performance = getPerformance();
   private t = Infinity;
 
+  public constructor() {
+    if (window) this.performance = window.performance;
+    else if (global && require) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      this.performance = require("perf_hooks").performance;
+    }
+  }
+
   public static isAvailable(): boolean {
-    return !!global?.performance;
+    return !!getPerformance();
   }
 
   public start() {
-    this.t = performance.now();
+    this.t = this.performance.now();
   }
 
   public stop(): number {
-    return this.t - performance.now();
+    return this.t - this.performance.now();
   }
 }
