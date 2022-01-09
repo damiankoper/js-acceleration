ab7cd32d8668144a6c74caa144c8ed7b2934ec5a
damiankoper <kopernickk@gmail.com>
2022-01-09 21:45:51 +0100

    feat: cpu benchmark

---
+++ packages/cpp-sequential/benchmark/threshold.cpp
@@ -0,0 +1,58 @@
+#include <benchmark/benchmark.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "./include/stb_image.h"
+#include "SHTSimple.h"
+#include "SHTSimpleLookup.h"
+
+std::string imageNames[] = {
+    //
+    "../../test/threshold/1.jpg",
+    "../../test/canny/1.jpg",
+    "../../test/simple/1.png",
+    "../../test/simple/2.png",
+    "../../test/simple/3.png",
+    "../../test/simple/4.png"
+    //
+};
+
+uint32_t width;
+uint8_t *rgb_image;
+std::vector<uint8_t> binaryImage;
+
+static void DoSetup(const benchmark::State &state) {
+  int width, height, bpp;
+  rgb_image =
+      stbi_load(imageNames[state.range(0)].c_str(), &width, &height, &bpp, 3);
+  ::width = width;
+  int length = width * height;
+
+  binaryImage = std::vector<uint8_t>(length / 3);
+  for (uint32_t i = 0; i < length; i += 3) {
+    binaryImage[i / 3] =
+        rgb_image[i] + rgb_image[i + 1] + rgb_image[i + 2] > 0 ? 1 : 0;
+  }
+}
+
+static void DoTeardown(const benchmark::State &state) {
+  stbi_image_free(rgb_image);
+}
+
+static void SHT_Simple(benchmark::State &state) {
+  for (auto _ : state)
+    SHTSimple(binaryImage, {width, {1.f, 1.f}, 0.75f});
+}
+BENCHMARK(SHT_Simple)
+    ->DenseRange(0, 5, 1)
+    ->Setup(DoSetup)
+    ->Teardown(DoTeardown);
+
+static void SHT_Simple_Lookup(benchmark::State &state) {
+  for (auto _ : state)
+    SHTSimpleLookup(binaryImage, {width, {1.f, 1.f}, 0.75f});
+}
+BENCHMARK(SHT_Simple_Lookup)
+    ->DenseRange(0, 5, 1)
+    ->Setup(DoSetup)
+    ->Teardown(DoTeardown);
+
+BENCHMARK_MAIN();
