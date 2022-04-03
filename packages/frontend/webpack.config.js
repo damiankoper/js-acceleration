0633e6fb243db47b4b340b0d24c17a859bcc4702
damiankoper <kopernickk@gmail.com>
2022-04-04 00:21:00 +0200

    feat: frontend template

---
+++ packages/frontend/webpack.config.js
@@ -0,0 +1,85 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+import { resolve, dirname } from "path";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { VueLoaderPlugin } from "vue-loader";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const config = {
+  entry: "./src/index.ts",
+  devtool: false,
+  output: {
+    path: resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+    static: { directory: resolve(__dirname, "static") },
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      scriptLoading: "module",
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new VueLoaderPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.tsx?$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+        options: {
+          // disable type checker - we will use it in fork plugin
+          transpileOnly: true,
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    fallback: {
+      buffer: false,
+      util: false,
+      zlib: false,
+      assert: false,
+      stream: false,
+      perf_hooks: false,
+    },
+  },
+};
+
+export default () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
